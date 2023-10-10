cfe7cde5fef5869baefb17f6de5053483ee63eb1
0xSYJiK <sanjari.xz@gmail.com>
2023-10-10 17:09:46 +0330

    a

---
+++ app.js
@@ -0,0 +1,7 @@
+var img = document.getElementById("example"); 
+img.addEventListener("click", function() { 
+   // code to execute when the image is clicked 
+}); 
+img.addEventListener("click", function() { 
+    window.location = "/index.html"; 
+ });
