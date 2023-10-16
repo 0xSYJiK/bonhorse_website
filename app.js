d50aeaebbcecc55ac95391d9cdf7ef81c0c37725
0xSYJiK <sanjari.xz@gmail.com>
2023-10-16 17:54:52 +0330

    commit2

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+
+
 var img = document.getElementById("example"); 
 img.addEventListener("click", function() { 
    // code to execute when the image is clicked 
@@ -5,3 +15,35 @@ img.addEventListener("click", function() {
 img.addEventListener("click", function() { 
     window.location = "/index.html"; 
  });
+
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshInterval = setInterval(()=> {next.click()}, 8000);
+function reloadSlider(){
+    slider.style.left = -items[active].offsetLeft + 'px';
+    // 
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 8000);
+
+    
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+         active = key;
+         reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
